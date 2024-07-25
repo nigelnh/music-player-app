0a3aa58eb2bc9653ae578a23c6e48e72a405e875
nigelnh <121311174+nigelnh@users.noreply.github.com>
2024-07-25 14:32:53 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Nhân Nguyễn on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
